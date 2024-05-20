1b05fa9e483e5c7dd243979016a26949cc906dcf
Igor Semyonov <igor@semyonov.xyz>
2024-05-20 14:18:52 -0400

    finished iterator exercise

--- exercise/closures_iterators/src/main.rs
+++ exercise/closures_iterators/src/main.rs
@@ -1,51 +1,111 @@
-// Yes, yes, we know. It's an exercise, compiler, we want it that way!
+// Yes, yes, we know. It's an exercise, compiler, we want it
+// that way!
 #[allow(unused_mut)]
 
 fn main() {
-    // 1. Uncomment the code below. Create a closure that returns the square of an integer (the
-    // number multiplied by itself), and assign the closure to the "square" variable. Then run the
+    // 1. Uncomment the code below. Create a closure that
+    //    returns the square of an integer (the
+    // number multiplied by itself), and assign the closure
+    // to the "square" variable. Then run the
     // code and make sure it works.
 
-    // let square = ...
-    // println!("5 squared is {}", square(5));
+    let square = |i: i32| i.pow(2);
+    println!(
+        "5 squared is {}",
+        square(5)
+    );
 
-    // 2. Uncomment the code below.  Finish the .map() iterator adaptor call by passing it a closure
-    // which takes a tuple of two integers as a parameter, and returns a tuple with the first
-    // integer incremented by 1, and the second integer left alone.  For example, if given the input
-    // (0, 1), it should return (1, 1). Run the code and make sure it works.
+    // 2. Uncomment the code below.  Finish the .map()
+    //    iterator adaptor call by passing it a closure
+    // which takes a tuple of two integers as a parameter,
+    // and returns a tuple with the first
+    // integer incremented by 1, and the second integer left
+    // alone.  For example, if given the input
+    // (0, 1), it should return (1, 1). Run the code and
+    // make sure it works.
 
-    // let pairs = vec![(0, 1), (2, 3), (4, 5)];
-    // pairs
-    //     .into_iter()
-    //     .map( ... )
-    //     .for_each(|t| println!("{:?}", t));
+    let pairs = vec![(0, 1), (2, 3), (4, 5)];
+    pairs
+        .clone()
+        .into_iter()
+        .map(
+            |(i, j)| {
+                (
+                    i + 1,
+                    j,
+                )
+            },
+        )
+        .for_each(|t| {
+            println!(
+                "{:?}",
+                t
+            )
+        });
 
-    // 3. Uncomment the code below. There is a mutable vector named `numbers`. Use an iterator over
-    // mutable references to multiply each of the values in `numbers` by 3.
-    // Hint 1: You'll need .iter_mut() -- bonus points if you use the shorter, syntactic sugar form!
-    // Hint 2: `x` will be a mutable reference, so remember to dereference it to use it
+    // 3. Uncomment the code below. There is a mutable
+    //    vector named `numbers`. Use an iterator over
+    // mutable references to multiply each of the values in
+    // `numbers` by 3. Hint 1: You'll need .iter_mut()
+    // -- bonus points if you use the shorter, syntactic
+    // sugar form! Hint 2: `x` will be a mutable
+    // reference, so remember to dereference it to use it
 
-    // let mut numbers = vec![1, 2, 3, 4];
-    // for x in ... {
-    //     ... // multiply the value by 3 via the mutable reference x
-    // }
-    // println!("{:?}", numbers); // should print [3, 6, 9, 12]
+    let mut numbers = vec![1, 2, 3, 4];
+    numbers
+        .iter_mut()
+        .for_each(|x| {
+            *x *= 3;
+        });
+    println!(
+        "{:?}",
+        numbers
+    ); // should print [3, 6, 9,12]
 
-    // 4. Uncomment the code below.  Take the vector of words and
-    // - Convert the vector into an iterator with .into_iter()
-    // - Use .filter() to remove any word that contains the letter "h" -- use .contains()
-    // - Use .map() to convert all the words to uppercase -- use .to_uppercase()
-    // - Use .collect() to put the transformed words back into a vector
+    // 4. Uncomment the code below.  Take the vector of
+    //    words and
+    // - Convert the vector into an iterator with
+    //   .into_iter()
+    // - Use .filter() to remove any word that contains the
+    //   letter "h" -- use .contains()
+    // - Use .map() to convert all the words to uppercase --
+    //   use .to_uppercase()
+    // - Use .collect() to put the transformed words back
+    //   into a vector
     //
-    // Hint: .to_uppercase() is a method on `str` which returns a String
+    // Hint: .to_uppercase() is a method on `str` which
+    // returns a String
 
-    // let words = vec!["autobot", "beach", "car", "decepticon", "energon", "frothy"];
-    // let transformed...  // do the stuff here
-    // println!("Transformed: {:?}", transformed);
+    let words = vec![
+        "autobot",
+        "beach",
+        "car",
+        "decepticon",
+        "energon",
+        "frothy",
+    ];
+    let transformed = words.into_iter()
+        .filter(|word| !word.contains("h"))
+        .map(|word| word.to_uppercase())
+        .collect::<Vec<_>>();
+    println!(
+        "Transformed: {:?}",
+        transformed
+    );
 
     // Challenge:
     //
     // - Rewrite the code in #2 as a for loop
-    // - Rewrite the code in #3 in functional style (without a for loop).  Hint: There are multiple
-    // ways to accomplish this, but they all end with an iterator consumer.
+    // had to add .clone() to the original call to allow this to work because pairs had been
+    // dropped due to use of into_iter
+    for (i, j) in pairs {
+        println!("{:?}", (i + 1, j));
+    }
+    // - Rewrite the code in #3 in functional style (without
+    //   a for loop).  Hint: There are multiple
+    // ways to accomplish this, but they all end with an
+    // iterator consumer.
+    // oops, already did this. I didn't want to leave a for loop running around
+
+
 }
