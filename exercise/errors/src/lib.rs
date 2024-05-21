ddcd7efc1c3b9d2b4a65bffa66217d6b0e797683
Igor Semyonov <igor@semyonov.xyz>
2024-05-20 21:56:38 -0400

    finished errors exercise

--- exercise/errors/src/lib.rs
+++ exercise/errors/src/lib.rs
@@ -1,20 +1,56 @@
-// 1. Create a DolphinError type representing the following three conditions:
+// 1. Create a DolphinError type representing the following
+//    three conditions:
 // - Hungry - The dolphin is hungry
 // - TooYoung - The dolphin is too young
-// - LongName - The dolphin's name is too long and annoying to say
+// - LongName - The dolphin's name is too long and annoying
+//   to say
 //
-// As a reminder, here are the 5 Guidelines for creating an error type:
-// (1) Use an `enum` for your error type
-// (2) Your error conditions should be enum variants grouped in as few enums as makes sense
-// (3) Don't expose error types other than your own (not going to be a problem for this exercise)
-// (4) Make your enum non-exhaustive
+// As a reminder, here are the 5 Guidelines for creating an
+// error type: (1) Use an `enum` for your error type
+// (2) Your error conditions should be enum variants grouped
+// in as few enums as makes sense (3) Don't expose error
+// types other than your own (not going to be a problem for
+// this exercise) (4) Make your enum non-exhaustive
 // (5) Implement the Debug, Display, and Error traits
-// (5b) You can use thiserror's `Error` macro to derive the Display and Error traits
+// (5b) You can use thiserror's `Error` macro to derive the
+// Display and Error traits
 //
-// Once you have completed defining the error type correctly, you should be able to run
-// `cargo build --lib` without any build errors or warnings. Then go to main.rs and continue with #2
+// Once you have completed defining the error type
+// correctly, you should be able to run `cargo build --lib`
+// without any build errors or warnings. Then go to main.rs
+// and continue with #2
 
-// pub enum DolphinError...
+#[derive(Debug)]
+pub enum DolphinError {
+    Hungry,
+    TooYoung,
+    LongName,
+}
+
+impl std::fmt::Display for DolphinError {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter,
+    ) -> std::fmt::Result {
+        use DolphinError::*;
+        match self {
+            Hungry => write!(
+                f,
+                "This mammal is hungry!"
+            ),
+            TooYoung => write!(
+                f,
+                "This mammal is way too young!"
+            ),
+            LongName => write!(
+                f,
+                "This mammal's name is too long!"
+            ),
+        }
+    }
+}
+
+impl std::error::Error for DolphinError {}
 
 pub struct Dolphin {
     pub name: String,
@@ -23,11 +59,22 @@ pub struct Dolphin {
 }
 
 impl Dolphin {
-    pub fn say_your_name(&self) -> Result<String, DolphinError> {
-        if self.name.len() > 10 {
+    pub fn say_your_name(
+        &self,
+    ) -> Result<String, DolphinError> {
+        if self
+            .name
+            .len()
+            > 10
+        {
             Err(DolphinError::LongName)
         } else {
-            Ok(format!("Hi, my name is {} and I'm a Dolphin!", self.name))
+            Ok(
+                format!(
+                    "Hi, my name is {} and I'm a Dolphin!",
+                    self.name
+                ),
+            )
         }
     }
     pub fn flip(&self) -> Result<String, DolphinError> {
@@ -37,11 +84,17 @@ impl Dolphin {
             Ok(format!("Yippee, I'm doing a flip!"))
         }
     }
-    pub fn shake_hands(&self) -> Result<String, DolphinError> {
+    pub fn shake_hands(
+        &self,
+    ) -> Result<String, DolphinError> {
         if self.hungry {
             Err(DolphinError::Hungry)
         } else {
-            Ok(format!("Nice to meet you, let's shake hands!"))
+            Ok(
+                format!(
+                    "Nice to meet you, let's shake hands!"
+                ),
+            )
         }
     }
 }
